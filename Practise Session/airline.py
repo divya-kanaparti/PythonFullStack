fceabf8f3d2e4a3e184febe94dfc28874e62b7ca
divya-kanaparti <divyakanaparti@gmail.com>
2026-08-22 17:27:27 +0530

    Practise

--- Practise Session/airline.py
+++ Practise Session/airline.py
@@ -2,20 +2,26 @@ base=5000
 amt=base
 seat=input("Enter type of seat: ")
 day=int(input("Enter no of days: "))
-festival=input()
+festival=input().lower()=='true'
 age=int(input("Enter age: "))
-if festival=="True":
+if festival:
     amt+=amt*0.2
 if seat=="Business":
     amt+=amt*0.4
+    # amt*=1.4
 elif seat=="Premium Economy":
     amt+=amt*0.2
+    # amt*=1.2
 else:
     pass
 if day>30:
     amt-=amt*0.1
-if day<7:
+    # amt*=0.9
+elif day<7:
     amt+=amt*0.25
-if age>=60:
+    # amt*=1.25
+if age>60:
     amt-=amt*0.15
 print(amt)
+
+
