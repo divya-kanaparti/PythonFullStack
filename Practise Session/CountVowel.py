df7b68d99c3b67e7a9cd4a39b48edb60a2108f48
divya-kanaparti <divyakanaparti@gmail.com>
2026-09-03 16:25:48 +0530

    Practise

--- Practise Session/CountVowel.py
+++ Practise Session/CountVowel.py
@@ -5,9 +5,10 @@ vowel="aeiou"
 count=0
 count2=0
 for ch in str:
-    if ch.lower() in vowel:
-        count+=1
-    else:
-        count2+=1
+    if ch.isalpha():
+        if ch.lower() in vowel:
+            count += 1
+        else:
+            count2 += 1
 print("Vowels:",count)
 print("Consonants:",count2)
---
+++ Practise Session/ReverseString.py
@@ -0,0 +1,6 @@
+str=input()
+def rev_str(str):
+    rev=""
+    rev=str[::-1]
+    return rev 
+print(rev_str(str))
